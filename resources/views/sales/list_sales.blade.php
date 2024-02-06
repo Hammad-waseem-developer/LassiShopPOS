47fde95c80a807cb36e638ff962dbf2986b3b2dd
Rao Talha <talharao997az@gmail.com>
2024-02-07 02:25:39 +0500

    list_sale print set

--- resources/views/sales/list_sales.blade.php
+++ resources/views/sales/list_sales.blade.php
@@ -25,6 +25,8 @@
             <a href="/sale/sales/create" class="btn btn-outline-primary btn-rounded btn-md m-1"><i
               class="i-Add me-2 font-weight-bold"></i>
             {{ __('translate.Create') }}</a>
+            <button id="printButton" class="btn btn-outline-success ms-3 fw-bolder" ><i
+              class="i-Add me-2 font-weight-bold"></i>Print</button>
             @endcan
             <a class="btn btn-outline-success btn-rounded btn-md m-1" id="Show_Modal_Filter"><i
               class="i-Filter-2 me-2 font-weight-bold"></i>
@@ -440,17 +442,17 @@
                         extend: 'collection',
                         text: "{{ __('translate.EXPORT') }}",
                         buttons: [
-                          {
-                            extend: 'print',
-                            text: 'Print',
-                            exportOptions: {
-                                columns: ':visible:Not(.not_show)',
-                                rows: ':visible'
-                            },
-                            title: function(){
-                                return 'Sales List';
-                            },
-                          },
+                          // {
+                          //   extend: 'print',
+                          //   text: 'Print',
+                          //   exportOptions: {
+                          //       columns: ':visible:Not(.not_show)',
+                          //       rows: ':visible'
+                          //   },
+                          //   title: function(){
+                          //       return 'Sales List';
+                          //   },
+                          // },
                           {
                             extend: 'pdf',
                             text: 'Pdf',
@@ -1088,6 +1090,30 @@
 
 </script>
 
+{{-- print --}}
+<script>
+  document.getElementById("printButton").addEventListener("click", function() {
+        var table = document.getElementById("sale_table");
+        if (table) {
+            // Clone the table
+            var tableClone = table.cloneNode(true);
 
+            // Exclude the "image" column
+            Array.from(tableClone.rows).forEach(function(row) {
+            row.deleteCell(0); // Remove the first column
 
+        });
+            
+            var newWin = window.open('', 'Print-Window');
+            newWin.document.open();
+            newWin.document.write('<html><head><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"></head><body>' + tableClone.outerHTML + '</body></html>');
+            newWin.document.close();
+            setTimeout(function() {
+                newWin.print();
+                newWin.close();
+            }, 10);
+        }
+    });
+
+  </script>
 @endsection
