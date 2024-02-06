57f1e0a216479c010017ee25d9c8dc593a7d6b7c
Hammad <hw13604@gmail.com>
2024-02-07 00:59:00 +0500

    Company Module Authorized !!

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -5,71 +5,85 @@ namespace App\Http\Controllers;
 use App\Models\Company;
 use Illuminate\Http\Request;
 use Session;
+
 class CompanyController extends Controller
 {
     public function index()
     {
         return view('hrm.company.index');
     }
-    
+
     public function create()
     {
-        return view('hrm.company.create');
+        if (auth()->user()->can('company_create')) {
+            return view('hrm.company.create');
+        }
+        return abort('403', __('You are not authorized'));
     }
     public function store(Request $request)
     {
-        $validatedData = $request->validate([
-            'name' => 'required|string|max:255',
-            'phone' => 'required|string|max:20',
-            'email' => 'required|string|max:200',
-            'country' => 'required|string|max:200',
-        ]);
-        $userId = auth()->id();
-        Company::create([
-            'user_id' => $userId,
-            'name' => $validatedData['name'],
-            'phone' => $validatedData['phone'],
-            'email' => $validatedData['email'],
-            'country' => $validatedData['country'],
-        ]);
-        return redirect()->route('company.index')->with('success', 'Company created successfully!');
+        if (auth()->user()->can('company_create')) {
+            $validatedData = $request->validate([
+                'name' => 'required|string|max:255',
+                'phone' => 'required|string|max:20',
+                'email' => 'required|string|max:200',
+                'country' => 'required|string|max:200',
+            ]);
+            $userId = auth()->id();
+            Company::create([
+                'user_id' => $userId,
+                'name' => $validatedData['name'],
+                'phone' => $validatedData['phone'],
+                'email' => $validatedData['email'],
+                'country' => $validatedData['country'],
+            ]);
+            return redirect()->route('company.index')->with('success', 'Company created successfully!');
+        }
+        return abort('403', __('You are not authorized'));
     }
 
     public function edit($id)
     {
-        $company = Company::findOrFail($id);
-        return view('hrm.company.edit', compact('company'));
+        if (auth()->user()->can('company_edit')) {
+            $company = Company::findOrFail($id);
+            return view('hrm.company.edit', compact('company'));
+        }
+        return abort('403', __('You are not authorized'));
     }
     public function update(Request $request, $id)
-{
-    $validatedData = $request->validate([
-        'name' => 'required|string|max:255',
-        'phone' => 'required|string|max:255',
-        'email' => 'required|string|email|max:255',
-        'country' => 'required|string|max:255',
-    ]);
-
-    $company = Company::findOrFail($id);
-    $company->update($validatedData);
-    return redirect()->route('company.index')->with('success', 'Company updated successfully!');
-}
-public function getData()
-{
-    $company = Company::get()->all();
- 
-    return response()->json($company);
-    
-}
-public function delete(Request $request)
     {
-        $company = Company::findOrFail($request->id);
-    
-        if ($company) {
-            $company->delete();
-            return response()->json(['message' => 'Company deleted successfully'], 200);
-        } else {
-            return response()->json(['message' => 'Company not found'], 404);
-        }
-    }
+        if (auth()->user()->can('company_edit')) {
+            $validatedData = $request->validate([
+                'name' => 'required|string|max:255',
+                'phone' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255',
+                'country' => 'required|string|max:255',
+            ]);
 
+            $company = Company::findOrFail($id);
+            $company->update($validatedData);
+            return redirect()->route('company.index')->with('success', 'Company updated successfully!');
+        }
+        return abort('403', __('You are not authorized'));
+    }
+    public function getData()
+    {
+        $company = Company::get()->all();
+
+        return response()->json($company);
+    }
+    public function delete(Request $request)
+    {
+        if (auth()->user()->can('company_delete')) {
+            $company = Company::findOrFail($request->id);
+
+            if ($company) {
+                $company->delete();
+                return response()->json(['message' => 'Company deleted successfully'], 200);
+            } else {
+                return response()->json(['message' => 'Company not found'], 404);
+            }
+        }
+        return abort('403', __('You are not authorized'));
+    }
 }
