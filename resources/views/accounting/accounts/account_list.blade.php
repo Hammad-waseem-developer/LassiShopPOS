931daed3d69a57f445ddad8730887c883621f0b1
Rao Talha <talharao997az@gmail.com>
2024-02-07 02:56:11 +0500

    account print set $ expense

--- resources/views/accounting/accounts/account_list.blade.php
+++ resources/views/accounting/accounts/account_list.blade.php
@@ -21,6 +21,8 @@
                   @can('account_add')
                   <a class="btn btn-outline-primary btn-md m-1" href="{{route('account.create')}}"><i
                       class="i-Add me-2 font-weight-bold"></i> {{ __('translate.Create') }}</a>
+                      <button id="printButton" class="btn btn-outline-success ms-3 fw-bolder" ><i
+                        class="i-Add me-2 font-weight-bold"></i>Print</button>
                   @endcan
                 </div>
                   <div class="table-responsive">
@@ -154,17 +156,17 @@
                         extend: 'collection',
                         text: "{{ __('translate.EXPORT') }}",
                         buttons: [
-                          {
-                            extend: 'print',
-                            text: 'Print',
-                            exportOptions: {
-                                columns: ':visible:Not(.not_show)',
-                                rows: ':visible'
-                            },
-                            title: function(){
-                                return 'Account List';
-                            },
-                          },
+                          // {
+                          //   extend: 'print',
+                          //   text: 'Print',
+                          //   exportOptions: {
+                          //       columns: ':visible:Not(.not_show)',
+                          //       rows: ':visible'
+                          //   },
+                          //   title: function(){
+                          //       return 'Account List';
+                          //   },
+                          // },
                           {
                             extend: 'pdf',
                             text: 'Pdf',
@@ -205,4 +207,30 @@
         
     });
 </script>
+{{-- print --}}
+<script>
+  document.getElementById("printButton").addEventListener("click", function() {
+        var table = document.getElementById("account_table");
+        if (table) {
+            // Clone the table
+            var tableClone = table.cloneNode(true);
+
+            // Exclude the "image" column
+            Array.from(tableClone.rows).forEach(function(row) {
+            row.deleteCell(3); // Remove the first column
+
+        });
+            
+            var newWin = window.open('', 'Print-Window');
+            newWin.document.open();
+            newWin.document.write('<html><head><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"></head><body>' + tableClone.outerHTML + '</body></html>');
+            newWin.document.close();
+            setTimeout(function() {
+                newWin.print();
+                newWin.close();
+            }, 10);
+        }
+    });
+
+  </script>
 @endsection
--- resources/views/accounting/deposit/deposit_list.blade.php
+++ resources/views/accounting/deposit/deposit_list.blade.php
@@ -21,6 +21,8 @@
                 @can('deposit_add')
                 <a class="btn btn-outline-primary btn-md m-1" href="{{route('deposit.create')}}"><i
                     class="i-Add me-2 font-weight-bold"></i> {{ __('translate.Create') }}</a>
+                    <button id="printButton" class="btn btn-outline-success ms-3 fw-bolder" ><i
+                      class="i-Add me-2 font-weight-bold"></i>Print</button>
                 @endcan
               </div>
               <div class="table-responsive">
@@ -160,17 +162,17 @@
                         extend: 'collection',
                         text: "{{ __('translate.EXPORT') }}",
                         buttons: [
-                          {
-                            extend: 'print',
-                            text: 'Print',
-                            exportOptions: {
-                                columns: ':visible:Not(.not_show)',
-                                rows: ':visible'
-                            },
-                            title: function(){
-                                return 'Deposit List';
-                            },
-                          },
+                          // {
+                          //   extend: 'print',
+                          //   text: 'Print',
+                          //   exportOptions: {
+                          //       columns: ':visible:Not(.not_show)',
+                          //       rows: ':visible'
+                          //   },
+                          //   title: function(){
+                          //       return 'Deposit List';
+                          //   },
+                          // },
                           {
                             extend: 'pdf',
                             text: 'Pdf',
@@ -211,4 +213,30 @@
         
     });
 </script>
+
+<script>
+  document.getElementById("printButton").addEventListener("click", function() {
+        var table = document.getElementById("table_deposit");
+        if (table) {
+            // Clone the table
+            var tableClone = table.cloneNode(true);
+
+            // Exclude the "image" column
+            Array.from(tableClone.rows).forEach(function(row) {
+            row.deleteCell(6); // Remove the first column
+
+        });
+            
+            var newWin = window.open('', 'Print-Window');
+            newWin.document.open();
+            newWin.document.write('<html><head><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"></head><body>' + tableClone.outerHTML + '</body></html>');
+            newWin.document.close();
+            setTimeout(function() {
+                newWin.print();
+                newWin.close();
+            }, 10);
+        }
+    });
+
+  </script>
 @endsection
