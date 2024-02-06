06ad9566988c4d8fb2d2b0a47344028225dd52a6
Rao Talha <talharao997az@gmail.com>
2024-02-07 02:18:04 +0500

    purchase print set

--- resources/views/purchases/list_purchases.blade.php
+++ resources/views/purchases/list_purchases.blade.php
@@ -23,6 +23,8 @@
             @can('purchases_add')
             <a href="/purchase/purchases/create" class="btn btn-outline-primary btn-md m-1"><i class="i-Add me-2 font-weight-bold"></i>
               {{ __('translate.Create') }}</a>
+              <button id="printButton" class="btn btn-outline-success ms-3 fw-bolder" ><i
+                class="i-Add me-2 font-weight-bold"></i>Print</button>
             @endcan
             <a class="btn btn-outline-success btn-md m-1" id="Show_Modal_Filter"><i class="i-Filter-2 me-2 font-weight-bold"></i>
               {{ __('translate.Filter') }}</a>
@@ -429,17 +431,17 @@
                         extend: 'collection',
                         text: "{{ __('translate.EXPORT') }}",
                         buttons: [
-                          {
-                            extend: 'print',
-                            text: 'Print',
-                            exportOptions: {
-                                columns: ':visible:Not(.not_show)',
-                                rows: ':visible'
-                            },
-                            title: function(){
-                                return 'Purchases List';
-                            },
-                          },
+                          // {
+                          //   extend: 'print',
+                          //   text: 'Print',
+                          //   exportOptions: {
+                          //       columns: ':visible:Not(.not_show)',
+                          //       rows: ':visible'
+                          //   },
+                          //   title: function(){
+                          //       return 'Purchases List';
+                          //   },
+                          // },
                           {
                             extend: 'pdf',
                             text: 'Pdf',
@@ -1063,7 +1065,31 @@
     })
 
 </script>
+{{-- prints --}}
+<script>
+  document.getElementById("printButton").addEventListener("click", function() {
+        var table = document.getElementById("purchase_table");
+        if (table) {
+            // Clone the table
+            var tableClone = table.cloneNode(true);
 
+            // Exclude the "image" column
+            Array.from(tableClone.rows).forEach(function(row) {
+            row.deleteCell(8); // Remove the first column
 
+        });
+            
+            var newWin = window.open('', 'Print-Window');
+            newWin.document.open();
+            newWin.document.write('<html><head><link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"></head><body>' + tableClone.outerHTML + '</body></html>');
+            newWin.document.close();
+            setTimeout(function() {
+                newWin.print();
+                newWin.close();
+            }, 10);
+        }
+    });
+
+  </script>
 
 @endsection
